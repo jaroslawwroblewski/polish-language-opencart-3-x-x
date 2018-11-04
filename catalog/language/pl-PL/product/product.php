6a21258752083f7726f82e84061cb5e70cc5d208
Jaroslaw Wroblewski <kontakt@d3sign.pl>
2018-11-04 18:48:36 +0100

    catalog - product - product

--- catalog/language/pl-PL/product/product.php
+++ catalog/language/pl-PL/product/product.php
@@ -1,50 +1,50 @@
 <?php
 // Text
-$_['text_search']              = 'Search';
-$_['text_brand']               = 'Brand';
-$_['text_manufacturer']        = 'Brand:';
-$_['text_model']               = 'Product Code:';
-$_['text_reward']              = 'Reward Points:';
-$_['text_points']              = 'Price in reward points:';
-$_['text_stock']               = 'Availability:';
-$_['text_instock']             = 'In Stock';
-$_['text_tax']                 = 'Ex Tax:';
-$_['text_discount']            = ' or more ';
-$_['text_option']              = 'Available Options';
-$_['text_minimum']             = 'This product has a minimum quantity of %s';
-$_['text_reviews']             = '%s reviews';
-$_['text_write']               = 'Write a review';
-$_['text_login']               = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
-$_['text_no_reviews']          = 'There are no reviews for this product.';
-$_['text_note']                = '<span class="text-danger">Note:</span> HTML is not translated!';
-$_['text_success']             = 'Thank you for your review. It has been submitted to the webmaster for approval.';
-$_['text_related']             = 'Related Products';
-$_['text_tags']                = 'Tags:';
-$_['text_error']               = 'Product not found!';
-$_['text_payment_recurring']   = 'Payment Profile';
+$_['text_search']              = 'Szukaj';
+$_['text_brand']               = 'Marka';
+$_['text_manufacturer']        = 'Producent:';
+$_['text_model']               = 'Kod produktu:';
+$_['text_reward']              = 'Punkty lojalnościowe:';
+$_['text_points']              = 'Cena w punktach lojalnościowych:';
+$_['text_stock']               = 'Dostępność:';
+$_['text_instock']             = 'W magazynie';
+$_['text_tax']                 = 'Podatek:';
+$_['text_discount']            = ' lub więcej ';
+$_['text_option']              = 'Dostępne opcje';
+$_['text_minimum']             = 'Minimalna ilość dla tego produktu to %s';
+$_['text_reviews']             = '%s opinii';
+$_['text_write']               = 'Napisz opinię';
+$_['text_login']               = 'Prosze się <a href="%s">zalogować</a> lub <a href="%s">zarejestrować</a>, aby przejżeć ten kontent.';
+$_['text_no_reviews']          = 'Ten produkt nie ma jeszcze opinii. Bądź pierwszy(a)!';
+$_['text_note']                = '<span class="text-danger">Uwaga:</span> kod HTML nie jest interpretowany!';
+$_['text_success']             = 'Dziękuje za udzieloną opinię. Wkrótce administrator sprawdzi czy nie posiada on wulgaryzmów.';
+$_['text_related']             = 'Polecane produkty';
+$_['text_tags']                = 'Tagi:';
+$_['text_error']               = 'Produkt nie znaleziony!';
+$_['text_payment_recurring']   = 'Profil płatności';
 $_['text_trial_description']   = '%s every %d %s(s) for %d payment(s) then';
 $_['text_payment_description'] = '%s every %d %s(s) for %d payment(s)';
 $_['text_payment_cancel']      = '%s every %d %s(s) until canceled';
-$_['text_day']                 = 'day';
-$_['text_week']                = 'week';
-$_['text_semi_month']          = 'half-month';
-$_['text_month']               = 'month';
-$_['text_year']                = 'year';
+$_['text_day']                 = 'dzień';
+$_['text_week']                = 'tydzień';
+$_['text_semi_month']          = 'pół miesiąca';
+$_['text_month']               = 'miesiąc';
+$_['text_year']                = 'rok';
 
 // Entry
-$_['entry_qty']                = 'Qty';
-$_['entry_name']               = 'Your Name';
-$_['entry_review']             = 'Your Review';
-$_['entry_rating']             = 'Rating';
-$_['entry_good']               = 'Good';
-$_['entry_bad']                = 'Bad';
+$_['entry_qty']                = 'Ilość';
+$_['entry_name']               = 'Nazwa';
+$_['entry_review']             = 'Twoja opinia';
+$_['entry_rating']             = 'Ocena';
+$_['entry_good']               = 'Dobry';
+$_['entry_bad']                = 'Zły';
 
 // Tabs
-$_['tab_description']          = 'Description';
-$_['tab_attribute']            = 'Specification';
-$_['tab_review']               = 'Reviews (%s)';
+$_['tab_description']          = 'Opis';
+$_['tab_attribute']            = 'Specyfikacja';
+$_['tab_review']               = 'Opinii (%s)';
 
 // Error
-$_['error_name']               = 'Warning: Review Name must be between 3 and 25 characters!';
-$_['error_text']               = 'Warning: Review Text must be between 25 and 1000 characters!';
-$_['error_rating']             = 'Warning: Please select a review rating!';
+$_['error_name']               = 'Uwaga: Nazwa musi zawierać od 3 do 25 znaków!';
+$_['error_text']               = 'Uwaga: Twoja opinia musi posiadać od 25 do 100 znaków!';
+$_['error_rating']             = 'Uwaga: Proszę wybierz ocenę!';
